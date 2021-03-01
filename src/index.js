6978cb5571c1a5d1dd72de57fb273ef34b1f1ed2
Diego Rodriguez <diego.rodriguez.labattaglia@gmail.com>
2021-03-01 14:21:58 -0300

    Added Inquirer.js as dependency

---
+++ src/index.js
@@ -0,0 +1,13 @@
+const inquirer = require('inquirer');
+
+//Basic questions
+
+inquirer
+	.prompt([
+		{
+			type: 'list',
+			name: 'mode',
+			message: 'What should I do?',
+			choices: ['Option A', 'Option B'],
+		},
+	])
