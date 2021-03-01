d2559367009b5f50d52d55c5f0a99cbc30411c0b
Diego Rodriguez <diego.rodriguez.labattaglia@gmail.com>
2021-03-01 14:28:10 -0300

    Eslint :D

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
-const inquirer = require('inquirer');
+const inquirer = require('inquirer')
 
 //Basic questions
 
 inquirer
-	.prompt([
-		{
-			type: 'list',
-			name: 'mode',
-			message: 'What should I do?',
-			choices: ['Option A', 'Option B'],
-		},
-	])
+    .prompt([
+        {
+            type: 'list',
+            name: 'mode',
+            message: 'What should I do?',
+            choices: ['Option A', 'Option B'],
+        },
+    ])
